6974caecc8a8cc510d6bdf50f19d8ae0bcddcad6
rishabhagrawal41 <rishabhagrawal41>
2020-07-08 21:38:50 +0530

    added points

--- game.py
+++ game.py
@@ -71,6 +71,11 @@ class Box:
     def draw(self, screen):
         pygame.draw.rect(screen, self.color,  (self.x+self.offset, self.y+self.offset, GRID_DIST-self.offset*2, GRID_DIST-self.offset*2))
 
+class Player:
+    def __init__(self, color, lcolor):
+        self.color = color
+        self.lcolor = lcolor
+        self.points = 0
 
 def main():
     pygame.init()
@@ -91,63 +96,69 @@ def main():
     points = []
     turn = True
 
+    player1 = Player(red, lred)
+    player2 = Player(blue, lblue)
+    curr_player = player1
+
     while not quit:
         clock.tick(clk_spd)
         screen.fill(white)
         pygame.font.init() 
-        myfont = pygame.font.SysFont('Comic Sans MS', 30)
-
-        if turn:
-            curr_color = red
-            curr_lcolor = lred
-        else:
-            curr_color = blue
-            curr_lcolor = lblue
+        myfont = pygame.font.SysFont('Comic Sans MS', 100)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit = True
-            # if event.type == pygame.KEYDOWN:
-            #     if event.key == pygame.K_Q:
-            #         quit = True
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if light_line not in lines: 
-                    light_line.change_color(curr_color)
+                    light_line.change_color(curr_player.color)
                     lines.append(light_line)
-                    turn = not turn
                     last = Dot(pos=light_line.start)
                     second_last = Dot(pos=light_line.stop)
+                    change_player = True
                     if last.y == second_last.y:
                         if second_last.x < last.x: last, second_last = second_last, last
-                        l1 = Line(curr_color, last.one_up(), second_last.one_up())
-                        l2 = Line(curr_color, second_last.one_up(), second_last.pos)
+                        l1 = Line(curr_player.color, last.one_up(), second_last.one_up())
+                        l2 = Line(curr_player.color, second_last.one_up(), second_last.pos)
                         l3 = light_line
-                        l4 = Line(curr_color, last.pos, last.one_up())
+                        l4 = Line(curr_player.color, last.pos, last.one_up())
                         if l1 in lines and l2 in lines and l3 in lines and l4 in lines:
-                            points.append(Box(curr_lcolor, l1.start))
+                            points.append(Box(curr_player.lcolor, l1.start))
+                            curr_player.points += 1
+                            change_player = False
 
-                        l1 = Line(curr_color, last.pos, second_last.pos)
-                        l2 = Line(curr_color, second_last.pos, second_last.one_down())
-                        l4 = Line(curr_color, second_last.one_down(), last.one_down())
-                        l3 = Line(curr_color, last.one_down(), last.pos)
-                            points.append(Box(curr_lcolor, l1.start))
+                        l1 = Line(curr_player.color, last.pos, second_last.pos)
+                        l2 = Line(curr_player.color, second_last.pos, second_last.one_down())
+                        l4 = Line(curr_player.color, second_last.one_down(), last.one_down())
+                        l3 = Line(curr_player.color, last.one_down(), last.pos)
+                        if l1 in lines and l2 in lines and l3 in lines and l4 in lines:
+                            points.append(Box(curr_player.lcolor, l1.start))
+                            curr_player.points += 1
+                            change_player = False
 
-                    if last.x == second_last.x:
+                    elif last.x == second_last.x:
                         if last.y < second_last.y: last, second_last = second_last, last
-                        l1 = Line(curr_color, second_last.pos, second_last.one_right())
-                        l2 = Line(curr_color, second_last.one_right(), last.one_right())
-                        l3 = Line(curr_color, last.one_right(), last.pos)
+                        l1 = Line(curr_player.color, second_last.pos, second_last.one_right())
+                        l2 = Line(curr_player.color, second_last.one_right(), last.one_right())
+                        l3 = Line(curr_player.color, last.one_right(), last.pos)
                         l4 = light_line
                         if l1 in lines and l2 in lines and l3 in lines and l4 in lines:
-                            points.append(Box(curr_lcolor, l1.start))
+                            points.append(Box(curr_player.lcolor, l1.start))
+                            curr_player.points += 1
+                            change_player = False
 
-                        l1 = Line(curr_color, second_last.one_left(), second_last.pos)
+                        l1 = Line(curr_player.color, second_last.one_left(), second_last.pos)
                         l2 = light_line
-                        l4 = Line(curr_color, last.pos, last.one_left())
-                        l3 = Line(curr_color, last.one_left(), second_last.one_left())
-                            points.append(Box(curr_lcolor, l1.start))
+                        l4 = Line(curr_player.color, last.pos, last.one_left())
+                        l3 = Line(curr_player.color, last.one_left(), second_last.one_left())
+                        if l1 in lines and l2 in lines and l3 in lines and l4 in lines:
+                            points.append(Box(curr_player.lcolor, l1.start))
+                            curr_player.points += 1
+                            change_player = False
             
+                    if change_player:
+                        curr_player = player1 if curr_player == player2 else player2
 
             
         def get_dist(p1, p2):
@@ -163,15 +174,15 @@ def main():
             while not((nearest.pos[0] == second_nearest.pos[0] or nearest.pos[1] == second_nearest.pos[1]) and get_dist(nearest.pos, second_nearest.pos) == GRID_DIST):
                 dist_point.remove(min(dist_point))
                 second_nearest = min(dist_point)[1]
-        light_line = Line(curr_lcolor, nearest.pos, second_nearest.pos)
+        light_line = Line(curr_player.lcolor, nearest.pos, second_nearest.pos)
         light_line.draw(screen)
         nearest.draw(screen)
         second_nearest.draw(screen)
 
-        textsurface = myfont.render(str(redp), False, red)
+        textsurface = myfont.render(str(player1.points), False, red)
         screen.blit(textsurface,(10,10))
-        textsurface = myfont.render(str(bluep), False, blue)
-        screen.blit(textsurface,(10,20))
+        textsurface = myfont.render(str(player2.points), False, blue)
+        screen.blit(textsurface,(10,80))
         for point in points: point.draw(screen)
         for line in lines: line.draw(screen)
         for dot in grid: dot.draw(screen)
